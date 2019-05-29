030e0379a17976aee1bca7738db173e61affc0a7
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-29 15:34:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,8 +53,8 @@ def checkout(cart, coupons)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   total = 0
-  final_cart.each do |name, properties|
-    total += properties[:price] * properties[:count]
+  final_cart.each do |name, attributes|
+    total += attributes[:price] * attributes[:count]
   end
   total = total * 0.9 if total > 100
   total
